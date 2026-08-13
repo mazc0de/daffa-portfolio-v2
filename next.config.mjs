755f49b344b9e7c3ce754130ff60414e30a15fac
mazc0de <dandroid36@gmail.com>
2026-08-13 13:02:38 +0700

    build: add cache-control header to disable caching

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {/* config options here */}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate'
+          }
+        ]
+      }
+    ]
+  }
+}
 
 export default nextConfig
